f4f5020d3663fe27291972d6dd417a95c951b1b3
thiagocascaes <thiagocascaes@outlook.com.br>
2025-05-19 13:33:56 -0300

    Testing commit

--- cypress/tests/sucesslogin.spec.ts
+++ cypress/tests/sucesslogin.spec.ts
@@ -2,7 +2,7 @@ describe('Login com sucesso', () => {
   it('Deve fazer login com um usuário válido', () => {
     cy.visit('http://localhost:3000/')
     cy.get('#username').type('thiagocascaes')
-    cy.get('#password').type('password123')
+    cy.get('#password').type('pass123456789')
     cy.get('[data-test="signin-submit"]').click()
   })
 })
